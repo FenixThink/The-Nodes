80f791b8a61595619de2efa606c860a1b23e6b65
jmoc3 <jmoc951@gmail.com>
2022-11-08 17:48:49 -0500

    maquetado con funcion enter

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
-
+let array = []
+let c = 0;
+let cDelete = 0;
 /* const city = document.querySelector(".#city").value */
 const log = (...sometigth)=>{
     console.log(sometigth)    
 }
 const makeHtml = (data)=>{
     log(data)
+    c++;
     const { main, name, sys, weather } = data
     const img = (weather[0].icon)
     const temperatura = main.temp
     const imagenName = weather[0].main
+    const card = document.getElementById("Card")
+    const container = document.createElement("div") 
     const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${img}.svg`
     const imagen = document.createElement("img")
     const cityName = document.createElement("p")
     const temp = document.createElement("p")
     const tempName = document.createElement("p")
     imagen.src = icon
-    document.getElementById("Card").appendChild(imagen)
-    document.getElementById("Card").appendChild(cityName)
-    document.getElementById("Card").appendChild(temp)
-    document.getElementById("Card").appendChild(tempName)
+    card.appendChild(container)
+    container.setAttribute("id", `div${c}`);
+    container.appendChild(imagen)
+    container.appendChild(cityName)
+    container.appendChild(temp)
+    container.appendChild(tempName)
     cityName.innerText = name
     temp.innerText = temperatura
     tempName.innerText = imagenName
+    array.push(container)
+    console.log(container);
+    console.log(array);
+    if (array.length>5) {
+        array.shift();
+        cDelete++;
+        document.getElementById(`div${cDelete}`).remove()
+        
+    }
+
 }
 const wheaterclim = (e)=>{
     e.preventDefault
@@ -40,3 +57,9 @@ const wheaterclim = (e)=>{
 }
 
 document.querySelector("#hola").addEventListener("click", wheaterclim)
+document.getElementById("city").addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        wheaterclim(e);
+    }
+    
+})
